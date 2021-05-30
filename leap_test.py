227b5b550e0bba6f86f12451645bdb967d5c4fff
KoreyEnglert <englertk@oregonstate.edu>
2021-05-29 19:46:11 -0700

    added another test to test simple leap years

--- leap_test.py
+++ leap_test.py
@@ -5,5 +5,8 @@ class TestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(leap.is_leap(1), False);
 
+    def test_add2(self):
+        self.assertEqual(leap.is_leap(4), True);
+
 if __name__ == '__main__':
     unittest.main()
