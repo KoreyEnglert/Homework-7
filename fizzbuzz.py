b80c03994cf392016f44dc2641d8e22a8f2eecbb
KoreyEnglert <englertk@oregonstate.edu>
2021-05-29 19:36:16 -0700

    the function has been updated to pass the third check and no longer passes the first two

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,10 @@
 def fizzbuzz():
     a = "";
     for x in range(1, 101, 1):
-        if(int(x/3) * 3 == x):
+        if(int(x/5) * 5 == x):
+            print("Buzz");
+            a = a + "Buzz ";
+        elif(int(x/3) * 3 == x):
             print("Fizz");
             a = a + "Fizz ";
         else:
