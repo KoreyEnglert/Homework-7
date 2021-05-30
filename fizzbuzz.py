6a285a61e2c6a432e289b9c9fc3c19732801d272
KoreyEnglert <englertk@oregonstate.edu>
2021-05-29 19:31:54 -0700

    the function has been updated to pass the new test and no longer passes the first test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,10 @@
 def fizzbuzz():
     a = "";
     for x in range(1, 101, 1):
-        print(x);
-        a = a + str(x) + " ";
+        if(int(x/3) * 3 == x):
+            print("Fizz");
+            a = a + "Fizz ";
+        else:
+            print(x);
+            a = a + str(x) + " ";
     return a;
