239d7d06b3e3dee42357dbf9034c46d415b87cda
KoreyEnglert <englertk@oregonstate.edu>
2021-05-29 19:40:24 -0700

    The function now works as intended and only passes the last test.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,10 @@
 def fizzbuzz():
     a = "";
     for x in range(1, 101, 1):
-        if(int(x/5) * 5 == x):
+        if(int(x/15) * 15 == x):
+            print("FizzBuzz");
+            a = a + "FizzBuzz ";
+        elif(int(x/5) * 5 == x):
             print("Buzz");
             a = a + "Buzz ";
         elif(int(x/3) * 3 == x):
