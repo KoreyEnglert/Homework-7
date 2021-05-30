2ce6e9e6874172d88ac2382daab80faf25bae110
KoreyEnglert <englertk@oregonstate.edu>
2021-05-29 19:51:46 -0700

    the function now passes all four tests passed

--- leap.py
+++ leap.py
@@ -2,7 +2,10 @@
 def is_leap(a):
     if(int(a / 4) * 4 == a):
         if(int(a / 100) * 100 == a):
-            return False;
+            if(int(a / 400) * 400 == a):
+                return True;
+            else:
+                return False;
         else:
             return True;
     else:
