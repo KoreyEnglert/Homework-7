d385caae13f79a53e8d6bcd2c983b672767b0339
KoreyEnglert <englertk@oregonstate.edu>
2021-05-29 19:48:31 -0700

    the function has now been updated to recognize complicated non-leap years (divisible by 100)

--- leap.py
+++ leap.py
@@ -1,6 +1,9 @@
 
 def is_leap(a):
     if(int(a / 4) * 4 == a):
-        return True;
+        if(int(a / 100) * 100 == 100):
+            return False;
+        else:
+            return True;
     else:
         return False;
